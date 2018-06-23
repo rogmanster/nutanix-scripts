908bf9e62505b1e22c1c3eb6eeeb9b8345c721f0
rogmanster <rogman@gmail.com>
2018-06-23 16:06:41 -0700

    changes

--- terraform/provider/nutanix/main.tf
+++ terraform/provider/nutanix/main.tf
@@ -1,7 +1,7 @@
 provider "nutanix" {
-  username = "admin"
-  password = "Nutanix/1234"
-  endpoint = "10.5.81.134"
+  username = "nutanix"
+  password = "Nutanix/4u!"
+  endpoint = "10.0.0.101"
   insecure = true
   port     = 9440
 }
@@ -16,12 +16,12 @@ resource "nutanix_virtual_machine" "my-machine" {
 
   cluster_reference = {
     kind = "cluster"
-    uuid = "000567f3-1921-c722-471d-0cc47ac31055"
+    uuid = "00056eda-31fe-0b6a-336d-001fc69be30e"
   }
 
   num_vcpus_per_socket = 1
   num_sockets          = 1
-  memory_size_mib      = 2048
+  memory_size_mib      = 512
   power_state          = "ON"
 
   nic_list = [{
@@ -29,7 +29,7 @@ resource "nutanix_virtual_machine" "my-machine" {
 
     subnet_reference = {
       kind = "subnet"
-      uuid = "af25e4af-12bd-4b1a-98ca-efcb0e9c06f8"
+      uuid = "d21dedcb-0b7b-4c4d-a3e6-30efe9378817"
     }
 
     network_function_nic_type = "INGRESS"
@@ -40,7 +40,7 @@ resource "nutanix_virtual_machine" "my-machine" {
       data_source_reference = [{
         kind = "image"
         name = "Centos7"
-        uuid = "9eabbb39-1baf-4872-beaf-adedcb612a0b"
+        uuid = "a866d321-b892-44c2-b7f8-12c8f0a8d33e"
       }]
 
       device_properties = [{
@@ -50,49 +50,6 @@ resource "nutanix_virtual_machine" "my-machine" {
       disk_size_mib = 1
     },
   ]
-
-  disk_list = [
-    {
-      data_source_reference = {
-        kind = "image"
-        name = "Centos7"
-        uuid = "9eabbb39-1baf-4872-beaf-adedcb612a0b"
-      }
-
-      device_properties = {
-        device_type = "DISK"
-      }
-
-      disk_size_mib = 1
-    },
-    {
-      data_source_reference = {
-        kind = "image"
-        name = "Centos7"
-        uuid = "9eabbb39-1baf-4872-beaf-adedcb612a0b"
-      }
-
-      device_properties = {
-        device_type = "DISK"
-      }
-
-      disk_size_mib = 1
-    },
-    {
-      data_source_reference = {
-        kind = "image"
-        name = "Centos7"
-        uuid = "9eabbb39-1baf-4872-beaf-adedcb612a0b"
-      }
-
-      device_properties = {
-        device_type = "DISK"
-      }
-
-      disk_size_mib = 1
-    },
-  ]
-}
 
 output "ip" {
   value = "${nutanix_virtual_machine.my-machine.ip_address}"
